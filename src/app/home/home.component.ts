a0f1ae97cd1dacee290204ba4edbacee81e3284b
Jarek Szczupak <dante1802@hotmail.com>
2024-10-22 09:21:57 +0200

    3rd

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule], // Dodaj HttpClientModule do importów
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  nextMatch: any;
+  matchesHistory: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('https://localhost:7193/MatchSchedule/next-match')
+      .subscribe(match => this.nextMatch = match);
+
+    this.http.get<any[]>('https://localhost:7193/Match/matches-history')
+      .subscribe(matches => this.matchesHistory = matches);
+  }
 }
