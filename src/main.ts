4f1cd63765335509ab5d2a6a4365dcd79e216709
Jarek Szczupak <dante1802@hotmail.com>
2024-10-22 09:43:55 +0200

    4th

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { LOCALE_ID } from '@angular/core';
+// Rejestracja lokalizacji polskiej
+registerLocaleData(localePl, 'pl');
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'pl' } // Ustawienie języka polskiego jako domyślnego
+  ]
+}).catch(err => console.error(err));
