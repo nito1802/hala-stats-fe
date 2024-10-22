a0c2ac0fa4e079ab132f14503505d1348826d6c5
Jarek Szczupak <dante1802@hotmail.com>
2024-10-22 12:14:52 +0200

    ranking

--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BaseUrl } from '../consts/urls';
+
+
+interface PlayerRankResponseDto {
+  index: number | null;
+  playerName: string;
+  eloRating: number;
+  gamesCount: number;
+}
 
 @Component({
   selector: 'app-ranking',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatButtonModule,
+    HttpClientModule
+  ],
   templateUrl: './ranking.component.html',
-  styleUrl: './ranking.component.css'
+  styleUrls: ['./ranking.component.css']
 })
-export class RankingComponent {
+export class RankingComponent implements OnInit {
+  displayedColumns: string[] = ['index', 'playerName', 'eloRating', 'gamesCount'];
+  players: PlayerRankResponseDto[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<PlayerRankResponseDto[]>(`${BaseUrl}/Player/rank`)
+      .subscribe(data => {
+        this.players = data;
+      });
+  }
 }
