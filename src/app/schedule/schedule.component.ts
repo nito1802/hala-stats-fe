a34b3b2ceb524e0fb6f83c063bcbc351e2132c1e
Jarek Szczupak <dante1802@hotmail.com>
2024-10-22 12:03:02 +0200

    schedule

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BaseUrl } from '../consts/urls';
+
+interface MatchScheduleResponseDto {
+  matchDate: Date;
+  skarbnikId: string;
+  state: string;
+  eventLink: string;
+}
 
 @Component({
   selector: 'app-schedule',
   standalone: true,
-  imports: [],
-  templateUrl: './schedule.component.html',
+  imports: [
+    CommonModule, // Wymagane dla dyrektyw Angulara, np. *ngIf, *ngFor
+    MatTableModule, // Importuj MatTableModule bezpośrednio do komponentu
+    MatButtonModule, // Importuj MatButtonModule bezpośrednio do komponentu
+    HttpClientModule
+  ],  templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.css'
 })
-export class ScheduleComponent {
+export class ScheduleComponent implements OnInit {
+  displayedColumns: string[] = ['index', 'matchDate', 'skarbnik', 'state', 'eventLink'];
+  matches: MatchScheduleResponseDto[] = [];
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.http.get<MatchScheduleResponseDto[]>(`${BaseUrl}/MatchSchedule/matches-schedule`)
+      .subscribe(data => {
+        this.matches = data;
+      });
+  }
 }
