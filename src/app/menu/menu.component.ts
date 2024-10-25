37cd7544e52ce5acbec21bdf72e7c70153fe481e
Jarek Szczupak <dante1802@hotmail.com>
2024-10-25 11:32:27 +0200

    menu responsywne

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar'; // Importuj MatToolbarModule
 import { RouterModule } from '@angular/router'; // Importuj RouterModule, jeśli korzystasz z routerLink
 @Component({
@@ -8,6 +8,23 @@ import { RouterModule } from '@angular/router'; // Importuj RouterModule, jeśli
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements AfterViewInit  {
+  ngAfterViewInit() {
+    const doc = document;
+    const menuOpen = doc.querySelector(".menu");
+    const menuClose = doc.querySelector(".close");
+    const overlay = doc.querySelector(".overlay");
+    const overlayContent = doc.querySelector(".overlay__content");
 
+    if (menuOpen && menuClose && overlay) {
+      menuOpen.addEventListener("click", () => {
+        overlay.classList.add("overlay--active");
+      });
+
+      menuClose.addEventListener("click", () => {
+        overlay.classList.remove("overlay--active");
+      });
+      //overlay__content
+    }
+  }
 }
