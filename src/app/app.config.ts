9c27763196dc7eec5c53ab9f8653e5c60b239901
Jarek Szczupak <dante1802@hotmail.com>
2026-02-24 13:07:54 +0100

    1.2.7 - dodanie analityki (na razie bez photo)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,52 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  APP_INITIALIZER,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { routes } from './app.routes';
+
+import {
+  provideMyShared,
+  UxTelemetryService,
+  RouteTelemetryService,
+  FocusTelemetryService,
+} from '@nito1802/my-shared';
+import { BaseUrl } from './consts/urls';
+
+// ✅ Factory z DI przez deps (bez inject())
+export function startTelemetryFactory(
+  ux: UxTelemetryService,
+  route: RouteTelemetryService,
+  focus: FocusTelemetryService,
+) {
+  return () => {
+    ux.start();
+    route.start();
+    focus.start();
+  };
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+
+    { provide: LOCALE_ID, useValue: 'pl' },
+
+    ...provideMyShared({ baseUrl: BaseUrl }),
+
+    {
+      provide: APP_INITIALIZER,
+      useFactory: startTelemetryFactory,
+      deps: [UxTelemetryService, RouteTelemetryService, FocusTelemetryService],
+      multi: true,
+    },
+  ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import { registerLocaleData } from '@angular/common';
 import localePl from '@angular/common/locales/pl';
 import { bootstrapApplication } from '@angular/platform-browser';
+
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { LOCALE_ID } from '@angular/core';
+import { appConfig } from './app/app.config';
+
 // Rejestracja lokalizacji polskiej
 registerLocaleData(localePl, 'pl');
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    { provide: LOCALE_ID, useValue: 'pl' } // Ustawienie języka polskiego jako domyślnego
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
