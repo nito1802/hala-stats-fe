b8f3a01a2170d3bbc35c2071d3d8f38016ef0ee5
Jarek Szczupak <dante1802@hotmail.com>
2026-04-20 15:08:46 +0200

    part 1

---
+++ src/app/models/season-awards.model.ts
@@ -0,0 +1,28 @@
+export interface SeasonAwardPlayerDto {
+  id: string;
+  displayName: string;
+}
+
+export interface SeasonStatsDto {
+  startAt: string;
+  endAt: string;
+  matchesCount: number;
+  timeElapsed: string;
+  goals: string;
+  highestWin: string;
+}
+
+export interface AwardDto {
+  id: number;
+  name: string;
+  seasonId: number;
+  createdAt: string;
+  players: SeasonAwardPlayerDto[];
+}
+
+export interface SeasonAwardsDto {
+  seasonIdx: number;
+  seasonName: string;
+  stats: SeasonStatsDto;
+  awards: AwardDto[];
+}
---
+++ src/app/services/season-awards.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { BaseUrl } from '../consts/urls';
+import { SeasonAwardsDto, AwardDto } from '../models/season-awards.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SeasonAwardsService {
+  constructor(private http: HttpClient) {}
+
+  getSeasonAwards(): Observable<SeasonAwardsDto[]> {
+    return this.http.get<SeasonAwardsDto[]>(`${BaseUrl}/Stats/season-awards`);
+  }
+
+  getSeasonByIdx(seasonIdx: number): Observable<SeasonAwardsDto | undefined> {
+    return this.getSeasonAwards().pipe(
+      map((seasons) => seasons.find((x) => x.seasonIdx === seasonIdx)),
+    );
+  }
+
+  getAwardDetails(
+    seasonIdx: number,
+    awardId: number,
+  ): Observable<{ season: SeasonAwardsDto; award: AwardDto } | undefined> {
+    return this.getSeasonAwards().pipe(
+      map((seasons) => {
+        const season = seasons.find((x) => x.seasonIdx === seasonIdx);
+        if (!season) {
+          return undefined;
+        }
+
+        const award = season.awards.find((x) => x.id === awardId);
+        if (!award) {
+          return undefined;
+        }
+
+        return { season, award };
+      }),
+    );
+  }
+}
